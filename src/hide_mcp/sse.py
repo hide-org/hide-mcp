fed3b363ac65fc921222cf79b44207f98e79d0f3
Art Moskvin <amoskvin@me.com>
2024-12-02 18:57:21 +0100

    fix sse

--- src/hide_mcp/sse.py
+++ src/hide_mcp/sse.py
@@ -7,13 +7,30 @@ from . import server
 sse = SseServerTransport("/messages")
 
 
-async def handle_sse(scope, receive, send):
-    async with sse.connect_sse(scope, receive, send) as streams:
+async def handle_sse(request):
+    async with sse.connect_sse(
+        request.scope, request.receive, request._send
+    ) as streams:
         await server.run_server(streams[0], streams[1])
 
 
-async def handle_messages(scope, receive, send):
-    await sse.handle_post_message(scope, receive, send)
+async def handle_messages(request):
+    # TODO: remove this hack once https://github.com/modelcontextprotocol/python-sdk/pull/83 is merged
+    # Create a dummy response that we'll return to Starlette
+    from starlette.responses import Response
+
+    response = Response("", status_code=202)
+
+    async def send_wrapper(message):
+        # Skip sending response since we're handling it at the Starlette level
+        if (
+            message["type"] != "http.response.start"
+            and message["type"] != "http.response.body"
+        ):
+            await request._send(message)
+
+    await sse.handle_post_message(request.scope, request.receive, send_wrapper)
+    return response
 
 
 starlette_app = Starlette(
